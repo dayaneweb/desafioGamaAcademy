60117ae0887906e3f11dbbb17cbbcf626f985b48
dayaneweb <dayanesousa007@gmail.com>
2021-07-16 20:50:01 -0300

    pequenas alterações na captura de valores

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function capturarValores()
 {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
-
+   
     localStorage.setItem("name", name);
     localStorage.setItem("email", email);
 
